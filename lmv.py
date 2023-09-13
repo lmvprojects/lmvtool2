8d800db20d54d777b3674b622b0e298327fc016f
lmvprojects <143235979+lmvprojects@users.noreply.github.com>
2023-09-13 11:02:31 +0545

    Update lmv.py

--- lmv.py
+++ lmv.py
@@ -179,43 +179,43 @@ O = '\x1b[1;96m'
 N = '\x1b[0m'    
 Z = "\033[1;30m"
 sir = '\033[41m\x1b[1;97m'
-x = '\33[m' # DEFAULT
-m = '\x1b[1;91m' #RED +
-k = '\033[93m' # KUNING +
-h = '\x1b[1;92m' # HIJAU +
-hh = '\033[32m' # HIJAU -
-u = '\033[95m' # UNGU
-kk = '\033[33m' # KUNING -
-b = '\33[1;96m' # BIRU -
-p = '\x1b[0;34m' # BIRU +
+x = '33[m' # DEFAULT
+m = 'x1b[1;91m' #RED +
+k = '033[93m' # YELLOW +
+h = 'x1b[1;92m' # GREEN +
+hh = '033[32m' # GREEN -
+u = '033[95m' # PURPLE
+kk = '033[33m' # YELLOW -
+b = '33[1;96m' # BLUE –
+p = 'x1b[0;34m' # BLUE +
 asu = random.choice([m,k,h,u,b])
 
 ###----------[ COLOUR STYLE ]---------- ###
 
-Z = "\x1b[0;90m"     # Hitam
-M = "\x1b[38;5;196m" # Merah
-H = "\x1b[38;5;46m"  # Hijau
-K = "\x1b[38;5;226m" # Kuning
-B = "\x1b[38;5;44m"  # Biru
-U = "\x1b[0;95m"     # Ungu
-O = "\x1b[0;96m"     # Biru Muda
-P = "\x1b[38;5;231m" # Putih
-J = "\x1b[38;5;208m" # Jingga
-A = "\x1b[38;5;248m" # Abu-Abu
+Z = "x1b[0;90m" # Black
+M = "x1b[38;5;196m" # Red
+h = "x1b[38;5;46m" # Green
+K = "x1b[38;5;226m" # Yellow
+B = "x1b[38;5;44m" # Blue
+U = "x1b[0;95m" # Purple
+O = "x1b[0;96m" # Light Blue
+P = "x1b[38;5;231m" # White
+J = "x1b[38;5;208m" # Orange
+A = "x1b[38;5;248m" # Grey
 
 ###----------[ RICH COLOR STYLE ]---------- ###
 
-Z2 = "[#000000]" # Hitam
-M2 = "[#FF0000]" # Merah
-H2 = "[#00FF00]" # Hijau
-K2 = "[#FFFF00]" # Kuning
-B2 = "[#00C8FF]" # Biru
-U2 = "[#AF00FF]" # Ungu
-N2 = "[#FF00FF]" # Pink
-O2 = "[#00FFFF]" # Biru Muda
-P2 = "[#FFFFFF]" # Putih
-J2 = "[#FF8F00]" # Jingga
-A2 = "[#AAAAAA]" # Abu-Abu
+Z2 = "[#000000]" # Black
+M2 = "[#FF0000]" # Red
+H2 = "[#00FF00]" # Green
+K2 = "[#FFFF00]" # Yellow
+B2="[#00C8FF]" # Blue
+U2 = "[#AF00FF]" # Purple
+N2="[#FF00FF]" #Pink
+O2 = "[#00FFFF]" # Light Blue
+P2 = "[#FFFFFF]" # White
+J2 = "[#FF8F00]" # Orange
+A2="[#AAAAAA]" # Gray
 
 #--------------------[ MONTH-CONVERTER ]--------------#
 
